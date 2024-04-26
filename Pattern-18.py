9ae924d45a0bbbe1c1f6ce0355e2177b12ef410a
GadirajuGunashree <167208145+GadirajuGunashree@users.noreply.github.com>
2024-04-26 10:29:03 +0530

    Create Pattern-18.py

---
+++ Pattern-18.py
@@ -0,0 +1,17 @@
+# pattern-18
+
+n=int(input())
+
+for i in range(1,n+1):
+    for j in range(i-1):
+        print(" ",end=" ")
+        
+    for j in range(2*(n-i)+1):
+        print("*",end=" ")
+    print()
+
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
