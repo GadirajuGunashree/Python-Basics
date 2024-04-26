f64b4c8575c8c7df2aad2b87276a01e0a315da64
GadirajuGunashree <167208145+GadirajuGunashree@users.noreply.github.com>
2024-04-26 10:21:43 +0530

    Create Pattern-13.py

---
+++ Pattern-13.py
@@ -0,0 +1,15 @@
+# pattern-13
+
+n = int(input())
+val = 10
+for i in range(1, n + 1):
+   for j in range(i):
+       print(val, end = " ")
+       val = val+10
+   print()
+
+#10
+#20 30
+#40 50 60
+#70 80 90 100
+#110 120 130 140 150
