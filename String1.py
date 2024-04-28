e8a318a8473e2746e463b0b623ea661fd33ad207
GadirajuGunashree <167208145+GadirajuGunashree@users.noreply.github.com>
2024-04-28 14:25:53 +0530

    Create String1.py

---
+++ String1.py
@@ -0,0 +1,16 @@
+# 1. Write a python program to accept a string and 
+#     print alternate characters starting 
+#     from first character
+ 
+# solution:
+    word = input()
+    n = len(word)
+    # n = 8
+    # 0 1 2 3 4 5 
+    for index in range(0, n, 2):
+        print(word[index], end = " ")
+
+# Input
+# mounika
+# Output
+# m u i a 
